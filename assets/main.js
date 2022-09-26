0959f7fecb77fa0a3352458b85718d9e1018282d
matheusOliv23 <matheushenri1996@gmail.com>
2022-09-25 22:23:42 -0300

    feat: adding logic to add new item to cart

--- assets/main.js
+++ assets/main.js
@@ -14,7 +14,56 @@ const products = [{
     },
 ];
 
-const cartItems = {};
+function addItem(produto) {
+    return {
+        type: 'ADICIONA_ITEM',
+        payload: produto
+    }
+}
+
+function removeItem(produto) {
+    return {
+        type: 'REMOVER_ITEM',
+        payload: {
+            id: produtoId
+        }
+    }
+}
+//estado inicial
+function reducer(state = {}, action) {
+    switch (action.type) {
+        case 'ADICIONA_ITEM':
+            return {
+                ...state,
+                [action.payload.id]: {
+                    ...action.payload,
+                    quantidade: state[action.payload.id] ? state[action.payload.id].quantidade + 1 : 1
+                }
+            }
+        case 'REMOVER_ITEM':
+            return state[action.payload.id].quantidade > 1 ? {
+                ...state,
+                [action.payload.id]: {
+                    ...state[action.payload.id],
+                    quantidade: state[action.payload.id].quantidade - 1
+                },
+            } : Object.keys(state).reduce(function(acumulador, produtoId) {
+                return {
+                    ...acumulador,
+
+                }
+            }, {})
+        default:
+            return state
+    }
+}
+
+const store = Redux.createStore(reducer)
+
+store.subscribe(function() {
+    renderizaTodosItemsCarrinho()
+    carrinhoValorTotal()
+})
 
 // Renderiza a estrutura do card do produto e suas informações
 function renderizaProduto(produto, index) {
@@ -39,13 +88,13 @@ function renderizaItem(produto) {
              <div class="carrinho_box">
                     <img class="carrinho_img" src="http://placeimg.com/640/480/arch" alt="Imagem aleatoria">
                     <div class="carrinho_detalhes">
-                        <div class="carrinho_produto-titulo"></div>
-                        <div class="carrinho_produto-preco">R$28</div>
-                        <div class="carrinho_qtd-item">Qtd:</div>
-                        <div class="carrinho_preco-total-item">Total</div>
+                        <div class="carrinho_produto-titulo">${produto.name}</div>
+                        <div class="carrinho_produto-preco">R$${produto.price}</div>
+                        <div class="carrinho_qtd-item">Qtd: ${produto.quantidade}</div>
+                        <div class="carrinho_preco-total-item">Total: ${produto.price * produto.quantidade}</div>
                         <input class="btn btn-primary" type="button" value="Adicionar">
                     </div>
-                    <input class="btn btn-danger btn-sm" type="button" value="Remover">
+                    <input data-produto-id="${produto.id}" class="btn btn-danger btn-sm btn-remove" type="button" value="Remover">
               </div>   
       </div>
   `;
@@ -61,31 +110,47 @@ function renderizaTodosProdutos() {
 }
 
 function renderizaTodosItemsCarrinho() {
+    const carrinhoItems = store.getState()
     let html = "";
-    for (let produtoId in cartItems) {
-        html = html + renderizaItem(cartItems[produtoId]);
+    for (let produtoId in carrinhoItems) {
+        html = html + renderizaItem(carrinhoItems[produtoId]);
     }
     document.querySelector(".lista_items").innerHTML = html;
 }
 
-function addItemCarrinho(produto) {
-    if (!cartItems[produto.id]) {
-        cartItems[produto.id] = produto;
-        cartItems[produto.id].amount = 0;
+function carrinhoValorTotal() {
+    const carrinhoItems = store.getState()
+    let total = 0;
+    for (let produtoId in carrinhoItems) {
+        total = total + (carrinhoItems[produtoId].price * carrinhoItems[produtoId].quantidade);
     }
-    ++cartItems[produto.id].amount
-    renderizaTodosItemsCarrinho();
+    document.querySelector(".total").innerHTML = `<h6>Total: <strong>R$${total}</strong></h6>`;
 }
+
+// function addItemCarrinho(produto) {
+//     if (!cartItems[produto.id]) {
+//         cartItems[produto.id] = produto;
+//         cartItems[produto.id].amount = 0;
+//     }
+//     ++cartItems[produto.id].amount
+// }
+
+function removerItemCarrinho() {}
 
 document.body.addEventListener("click", function(event) {
     const elemento = event.target;
     if (elemento.classList.contains("btn-add")) {
         const index = parseInt(elemento.getAttribute("data-index"));
         const produto = products[index];
+        store.dispatch(addItem(produto))
+    }
 
-        addItemCarrinho(produto);
+    if (elemento.classList.contains('btn-remove')) {
+        const produtoId = elemento.getAttribute('data-produto-id')
+        store.dispatch(removeItem(produtoId))
     }
 });
 document.querySelector(".lista_produtos").innerHTML = renderizaTodosProdutos();
 
 renderizaTodosProdutos();
+carrinhoValorTotal()
